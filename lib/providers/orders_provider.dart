0048dcafc50a9cb6ae8405c08859012b6a90a520
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2021-06-22 23:36:51 -0500

    orders view mejorada

--- lib/providers/orders_provider.dart
+++ lib/providers/orders_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'package:skyway_users/core/utilities/http_info.dart';
+import 'package:skyway_users/models/collections/order.dart';
 
 class OrdersProvider extends Bloc {
   OrdersProvider() : super(0);
@@ -20,5 +21,22 @@ class OrdersProvider extends Bloc {
     );
     if (response.statusCode == 200) return json.decode(response.body);
     return null;
+  }
+
+  Future<bool> update(OrderModel order) async {
+    final url = Uri.https(baseUri, "/api/orders/update");
+    print(order.toJson());
+    final response = await http.put(
+      url,
+      body: json.encode(order.toJson()),
+      headers: httpHeaders,
+    );
+    final ans = response.body;
+    print(ans);
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      if (ans == order.id) return true;
+    }
+    return false;
   }
 }
